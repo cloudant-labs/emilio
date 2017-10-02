abc3179732360788882f7c71874203e60f99a09b
Paul J. Davis <paul.joseph.davis@gmail.com>
2017-10-02 09:07:23 -0500

    Simplify and fix export indentation check

--- src/emilio_check_indents_exports.erl
+++ src/emilio_check_indents_exports.erl
@@ -36,77 +36,19 @@ format_error(130, Levels) ->
 
 
 run(Lines) ->
-    case find_export_group(Lines) of
-        {Export, RestLines} ->
-            check_export(Export),
-            run(RestLines);
-        not_found ->
+    emilio_lib:foreach_token(fun check_exports/3, Lines).
+
+
+check_exports(Anno, Token, Ctx) ->
+    case element(1, Token) of
+        export ->
+            CurrLine = emilio_lib:curr_line(Ctx),
+            case emilio_lib:indent_level(CurrLine) of
+                1 ->
+                    ok;
+                N ->
+                    ?EMILIO_REPORT(Anno, 130, N)
+            end;
+        _ ->
             ok
     end.
-
-
-check_export([_]) ->
-    % Single line attribute so no indentation
-    ok;
-
-check_export([_FirstLine | RestLines]) ->
-    {BodyLines, [_LastLine]} = lists:split(length(RestLines) - 1, RestLines),
-    lists:foreach(fun(Line) ->
-        case emilio_lib:is_blank_line(Line) of
-            true ->
-                ok;
-            false ->
-                case emilio_lib:indent_level(Line) of
-                    1 ->
-                        ok;
-                    N ->
-                        Anno = element(2, hd(Line)),
-                        ?EMILIO_REPORT(Anno, 130, N)
-                end
-        end
-    end, BodyLines).
-
-
-find_export_group([]) ->
-    not_found;
-
-find_export_group([Line | RestLines]) ->
-    case drop_before_export(Line) of
-        [] ->
-            find_export_group(RestLines);
-        Tokens ->
-            take_until_dot([Tokens | RestLines])
-    end.
-
-
-take_until_dot([Line | Rest]) ->
-    case has_dot(Line) of
-        true ->
-            {WithDot, RestLine} = split_after_dot(Line),
-            {WithDot, [RestLine | Rest]};
-        false ->
-            {WithDot, RestLines} = take_until_dot(Rest),
-            {[Line | WithDot], RestLines}
-    end.
-
-
-drop_before_export(Line) ->
-    Pred = fun(Token) ->
-        case Token of
-            {attribute, _, export} -> false;
-            _ -> true
-        end
-    end,
-    lists:dropwhile(Pred, Line).
-
-
-has_dot(Line) ->
-    lists:foldl(fun(Token, HasDot) ->
-        element(1, Token) == dot orelse HasDot
-    end, false, Line).
-
-
-split_after_dot(Line) ->
-    Pred = fun(Token) -> element(1, Token) /= dot end,
-    {Before, [Dot | After]} = lists:splitwith(Pred, Line),
-    {[Before ++ [Dot]], After}.
